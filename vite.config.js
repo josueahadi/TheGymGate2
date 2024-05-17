1f0af10366e47d3cfb7758e1c9d98352da8a05a6
josueahadi <ahadihjosue@gmail.com>
2024-05-17 11:57:52 +0200

    build

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true, // Ensure the output directory is emptied before building
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/index.html'),
+    },
+  },
+});
