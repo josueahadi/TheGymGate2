1b947163c3625b7555e392e2d7fe427eac88f6e1
josueahadi <ahadihjosue@gmail.com>
2024-05-17 21:28:07 +0200

    section: footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      alignItems: {
+        'first-baseline': 'first baseline',
+      },
       backgroundSize: {
         '41': '41%',
+        '50': '50%',
       },
       margin: {
         '-0.5em': '-0.5em',
       },
       backgroundPosition: {
         'x-100': '100%',
+        '-390px--65px': '-390px -65px',
       },
     },
   },
   plugins: [],
 }
-
